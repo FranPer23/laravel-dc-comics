13a08fa81c5585ddc964e85509edb17f0d0915b7
FranPer23 <francescaprt@yahoo.com>
2023-05-29 14:52:16 +0200

    first commit

---
+++ database/migrations/2023_05_29_123646_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 30);
+            $table->text('description')->nullable();
+            $table->string('type', 30);
+            $table->text('thumb');
+            $table->decimal('price', 8, 2);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->timestamps();
+            $table->save();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
