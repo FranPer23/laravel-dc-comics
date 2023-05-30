256382da7cefd53c67e940e50dd1642a0cd636c4
FranPer23 <francescaprt@yahoo.com>
2023-05-30 17:34:33 +0200

    popolamento tabella db

--- database/migrations/2023_05_29_123646_create_comics_table.php
+++ database/migrations/2023_05_29_123646_create_comics_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 30);
+            $table->string('title', 50);
             $table->text('description')->nullable();
             $table->string('type', 30);
             $table->text('thumb');
-            $table->decimal('price', 8, 2);
-            $table->string('series', 30);
+            $table->string('price', 10);
+            $table->string('series', 100);
             $table->date('sale_date');
             $table->timestamps();
         });
